cc63d214195e59953d89fecfa7ae66c472cafd5f
cabeto123 <carlberto153@gmail.com>
2025-02-07 16:46:41 -0600

    Clase empleados

--- ProyectoEstru2/Empleados.h
+++ ProyectoEstru2/Empleados.h
@@ -1,5 +1,20 @@
 #pragma once
+#include <string>
+using namespace std;
 class Empleados
 {
+	private:
+		int id;
+		int salario;
+		bool estado;
+	public:
+		string nombre,departamento, puesto;
+		int getid();
+		int getsalario();
+		bool getestado();
+		void setid(int id);
+		void setsalario(int salario);
+		void setestado(bool estado);
+
 };
 
