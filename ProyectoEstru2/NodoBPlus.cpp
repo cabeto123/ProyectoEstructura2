5a53d98ae5d7358399f839e7853c26496369b613
cabeto123 <carlberto153@gmail.com>
2025-02-09 08:54:53 -0600

    Menu

--- ProyectoEstru2/NodoBPlus.cpp
+++
@@ -1,1 +0,0 @@
-#include "NodoBPlus.h"
--- ProyectoEstru2/ProyectoEstru2.cpp
+++ ProyectoEstru2/ProyectoEstru2.cpp
@@ -4,8 +4,34 @@
 #include "Pedido.h"
 #include "ArbolBPlus.h"
 using namespace std;
+void gestion_empleados() {
+
+}
 int main() {
-   
+    int opcion = 1;
+    while (opcion!=6) {
+        cout << "1.Gestion de empleados"<<endl;
+        cout << "2.Gestion de inventarios" << endl;
+        cout << "3.Gestion de clientes" << endl;
+        cout << "4.Gestion de ventas" << endl;
+        cout << "5.Gestion de pedidos" << endl;
+        cout << "Opcion: ";
+        cin >> opcion;
+        switch (opcion)
+        {
+        case 1:
+            gestion_empleados();
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        }
+    }
 
     return 0;
 }
